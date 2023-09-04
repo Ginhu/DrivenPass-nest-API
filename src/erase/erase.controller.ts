5845205a8b017fa548497570605454ed7d28f38c
Sergio Beppu <beppu.sergio@gmail.com>
2023-09-04 14:12:49 -0300

    swagger documentation

--- src/erase/erase.controller.ts
+++ src/erase/erase.controller.ts
@@ -11,14 +11,27 @@ import { EraseDto } from './dto/erase.dto';
 import { User } from '../decorators/user.decorator';
 import { Users } from '@prisma/client';
 import { AuthGuard } from '../guards/auth.guard';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('erase')
 @UseGuards(AuthGuard)
+@ApiBearerAuth()
 export class EraseController {
   constructor(private readonly eraseService: EraseService) {}
 
   @Post()
   @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    summary: 'Delete user account and saved informations from the database',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'All data deleted from database',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'wrong confirmation password',
+  })
   remove(@Body() eraseDto: EraseDto, @User() user: Users) {
     return this.eraseService.remove(eraseDto, user);
   }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpStatus } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
 @ApiTags('Users')
@@ -9,6 +9,15 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('sign-up')
+  @ApiOperation({ summary: 'signup new user in the app' })
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'User signed up successfully',
+  })
+  @ApiResponse({
+    status: HttpStatus.CONFLICT,
+    description: 'email already registered in the app',
+  })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
